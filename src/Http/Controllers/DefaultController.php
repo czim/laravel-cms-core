330e2de62d32dcdb7b4ff1a1e8876350af7f0078
Coen Zimmerman <coen@pixelindustries.com>
2016-11-26 18:10:22 +0100

    Cleaned up default controller

--- src/Http/Controllers/DefaultController.php
+++ src/Http/Controllers/DefaultController.php
@@ -4,9 +4,15 @@ namespace Czim\CmsCore\Http\Controllers;
 class DefaultController extends Controller
 {
 
+    /**
+     * Shows a very boring blank page.
+     *
+     * Usable as a safe default fallback action.
+     *
+     * @return mixed
+     */
     public function index()
     {
-
         return view('cms::blank.index');
     }
 
