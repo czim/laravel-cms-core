cf6b62144d8e881bb51686181d37f5d9769d303e
Coen Zimmerman <coen@pixelindustries.com>
2017-03-05 17:44:44 +0100

    Added basic web controller tests

---
+++ tests/Http/Controllers/DefaultControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Czim\CmsCore\Http\Controllers;
+
+use Czim\CmsCore\Support\Enums\Component;
+use Czim\CmsCore\Test\CmsBootTestCase;
+use Illuminate\Contracts\View\Factory;
+
+class DefaultControllerTest extends CmsBootTestCase
+{
+
+    /**
+     * @test
+     */
+    function it_returns_a_default_blank_view()
+    {
+        $controller = new DefaultController($this->app[Component::CORE]);
+
+        $viewMock = $this->getMockBuilder(Factory::class)->getMock();
+        $viewMock->method('make')->with('cms::blank.index')->willReturn('testing');
+
+        $this->app->instance(Factory::class, $viewMock);
+
+        static::assertEquals('testing', $controller->index());
+    }
+
+}
---
+++ tests/Http/Controllers/LocaleControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace Czim\CmsCore\Http\Controllers;
+
+use Czim\CmsCore\Contracts\Support\Localization\LocaleRepositoryInterface;
+use Czim\CmsCore\Http\Requests\SetLocaleRequest;
+use Czim\CmsCore\Support\Enums\Component;
+use Czim\CmsCore\Test\CmsBootTestCase;
+use Illuminate\Http\RedirectResponse;
+
+class LocaleControllerTest extends CmsBootTestCase
+{
+
+    /**
+     * @test
+     */
+    function it_sets_an_available_locale()
+    {
+        /** @var LocaleRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject $repositoryMock */
+        /** @var SetLocaleRequest|\PHPUnit_Framework_MockObject_MockObject $requestMock */
+        $repositoryMock = $this->getMockBuilder(LocaleRepositoryInterface::class)->getMock();
+        $requestMock    = $this->getMockBuilder(SetLocaleRequest::class)->getMock();
+
+        $repositoryMock->expects(static::atLeastOnce())
+            ->method('isAvailable')
+            ->with('nl')
+            ->willReturn(true);
+
+        $requestMock->expects(static::atLeastOnce())
+            ->method('input')
+            ->with('locale')
+            ->willReturn('nl');
+
+        $controller = new LocaleController($this->app[Component::CORE], $repositoryMock);
+
+        /** @var RedirectResponse $return */
+        $return = $controller->setLocale($requestMock);
+
+        static::assertInstanceOf(RedirectResponse::class, $return);
+
+        static::assertEquals('nl', session()->get('cms::locale'));
+    }
+
+    /**
+     * @test
+     */
+    function it_redirects_if_locale_to_set_is_unavailable()
+    {
+        /** @var LocaleRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject $repositoryMock */
+        /** @var SetLocaleRequest|\PHPUnit_Framework_MockObject_MockObject $requestMock */
+        $repositoryMock = $this->getMockBuilder(LocaleRepositoryInterface::class)->getMock();
+        $requestMock    = $this->getMockBuilder(SetLocaleRequest::class)->getMock();
+
+        $repositoryMock->expects(static::atLeastOnce())
+            ->method('isAvailable')
+            ->with('nl')
+            ->willReturn(false);
+
+        $requestMock->expects(static::atLeastOnce())
+            ->method('input')
+            ->with('locale')
+            ->willReturn('nl');
+
+        $controller = new LocaleController($this->app[Component::CORE], $repositoryMock);
+
+        /** @var RedirectResponse $return */
+        $return = $controller->setLocale($requestMock);
+
+        static::assertInstanceOf(RedirectResponse::class, $return);
+
+        static::assertFalse(session()->has('cms::locale'));
+    }
+
+}
