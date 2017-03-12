8a00496f916050ed23665078f7cfdbabf4ddb0e2
Coen Zimmerman <coen@pixelindustries.com>
2017-03-12 14:00:06 +0100

    Re-enabled API with forked package for 5.4 support

--- config/cms-core.php
+++ config/cms-core.php
@@ -158,8 +158,8 @@ return [
         Czim\CmsCore\Providers\ViewServiceProvider::class,
         Czim\CmsAuth\Providers\CmsAuthServiceProvider::class,
         Czim\CmsTheme\Providers\CmsThemeServiceProvider::class,
-        //Czim\CmsAuth\Providers\Api\OAuthSetupServiceProvider::class,
-        //Czim\CmsCore\Providers\Api\CmsCoreApiServiceProvider::class,
+        Czim\CmsAuth\Providers\Api\OAuthSetupServiceProvider::class,
+        Czim\CmsCore\Providers\Api\CmsCoreApiServiceProvider::class,
 
         // It is safest to call the route service providers last,
         // since they depend on data provided by modules that
@@ -167,7 +167,7 @@ return [
         \Czim\CmsModels\Providers\CmsModelsServiceProvider::class,
 
         Czim\CmsCore\Providers\RouteServiceProvider::class,
-        //Czim\CmsCore\Providers\Api\ApiRouteServiceProvider::class,
+        Czim\CmsCore\Providers\Api\ApiRouteServiceProvider::class,
     ],
 
     /*
