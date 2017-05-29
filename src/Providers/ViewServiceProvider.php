ea02b0071252c614852d67532b3af32de9f0b95c
Coen Zimmerman <tabtemp@xs4all.nl>
2017-05-29 14:45:20 +0200

    Added blade directives
    @cms_script
    @cms_scriptonce
    @cms_style
    @cms_scriptasset

--- src/Providers/ViewServiceProvider.php
+++ src/Providers/ViewServiceProvider.php
@@ -1,8 +1,10 @@
 <?php
 namespace Czim\CmsCore\Providers;
 
+use Blade;
 use Czim\CmsCore\Contracts\Core\CoreInterface;
 use Czim\CmsCore\Http\ViewComposers\LocaleComposer;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
@@ -20,7 +22,9 @@ class ViewServiceProvider extends ServiceProvider
     {
         $this->core = $core;
 
-        $this->registerViewComposers();
+        $this
+            ->registerViewComposers()
+            ->registerBladeDirectives();
     }
 
     public function register()
@@ -40,6 +44,78 @@ class ViewServiceProvider extends ServiceProvider
         );
 
         return $this;
+    }
+
+    /**
+     * Registers blade directives for the CMS.
+     */
+    protected function registerBladeDirectives()
+    {
+        // Set up the cms_script directive
+        Blade::directive('cms_script', function () {
+            return "<?php ob_start(); ?>";
+        });
+
+        Blade::directive('cms_endscript', function () {
+            return "<?php app(Czim\\CmsCore\\Contracts\\Support\\View\\AssetManagerInterface::class)"
+                 . "->registerScript(ob_get_clean(), false); ?>";
+        });
+
+
+        // Set up the cms_scriptonce directive
+        Blade::directive('cms_scriptonce', function () {
+            return "<?php ob_start(); ?>";
+        });
+
+        Blade::directive('cms_endscriptonce', function () {
+            return "<?php app(Czim\\CmsCore\\Contracts\\Support\\View\\AssetManagerInterface::class)"
+                 . "->registerScript(ob_get_clean(), true); ?>";
+        });
+
+
+        // Set up the cms_scriptassethead directive
+        Blade::directive('cms_scriptassethead', function ($expression) {
+
+            if ($this->isBladeArgumentStringBracketed()) {
+                $expression = substr($expression, 1, -1);
+            }
+
+            return "<?php app(Czim\\CmsCore\\Contracts\\Support\\View\\AssetManagerInterface::class)"
+                . "->registerScriptAsset({$expression}, true); ?>";
+        });
+
+
+        // Set up the cms_scriptasset directive
+        Blade::directive('cms_scriptasset', function ($expression) {
+            return "<?php app(Czim\\CmsCore\\Contracts\\Support\\View\\AssetManagerInterface::class)"
+                . "->registerScriptAsset"
+                . ($this->isBladeArgumentStringBracketed() ? $expression : "({$expression})") . "; ?>";
+        });
+
+
+        // Set up the cms_style directive
+        Blade::directive('cms_style', function ($expression) {
+            return "<?php app(Czim\\CmsCore\\Contracts\\Support\\View\\AssetManagerInterface::class)"
+                . "->registerStyleAsset"
+                . ($this->isBladeArgumentStringBracketed() ? $expression : "({$expression})") . "; ?>";
+        });
+    }
+
+    /**
+     * Returns whether the blade directive argument brackets are included.
+     *
+     * This behaviour was changed with the release of Laravel 5.3.
+     *
+     * @return bool
+     */
+    protected function isBladeArgumentStringBracketed()
+    {
+        if ( ! preg_match('#^(?<major>\d+)\.(?<minor>\d+)(?:\..*)?$#', Application::VERSION, $matches)) {
+            return true;
+        }
+        $major = (int) $matches['major'];
+        $minor = (int) $matches['minor'];
+        return ($major < 5 || $major == 5 && $minor < 3);
     }
 
 }
